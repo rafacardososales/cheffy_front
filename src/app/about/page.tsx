248577aa566c830fa458ca32bfe1435e7aa4da7e
rafacardososales <rafael.moaz@yahoo.com.br>
2025-06-15 21:38:47 -0300

    feita config do prettier e do setting.json

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
-export default function  AboutPage(){
-    return <h1>essa e a pagina ABOUT</h1>
+import clsx from 'clsx';
+
+export default function AboutPage() {
+  return (
+    <h1 className={clsx(`text-3xl text-blue-500 font-bold w-full text-center`)}>
+      essa e a pagina ABOUT
+    </h1>
+  );
 }
