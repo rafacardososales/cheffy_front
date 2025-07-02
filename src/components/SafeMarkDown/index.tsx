b2c361233aafd3cd61d13c5694ed0615336e08d5
rafacardososales <rafael.moaz@yahoo.com.br>
2025-07-01 21:41:55 -0300

    feito alguns ajustes no markdown onde sera feita a descirção dos posts e instalado a lib tailwindcss/typography

--- src/components/SafeMarkDown/index.tsx
+++ src/components/SafeMarkDown/index.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
+import clsx from 'clsx';
 
 type SafeMarkDownProps = {
   markdown: string;
@@ -8,7 +9,19 @@ type SafeMarkDownProps = {
 
 export function SafeMarkDown({ markdown }: SafeMarkDownProps) {
   return (
-    <div>
+    <div
+      className={clsx(
+        'prose prose-slate',
+        ' w-full max-w-none',
+        ' overflow-hidden',
+        ' prose-a:no-underline',
+        ' prose-a:transition',
+        ' prose-a:text-blue-500',
+        ' prose-a:hover:text-blue-700',
+        'prose-img:mx-auto',
+        'md:prose-lg',
+      )}
+    >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
